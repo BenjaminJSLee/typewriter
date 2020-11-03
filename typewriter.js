f845733e29ddf08298a13e4459fcc40a2b8d0709
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-03 20:04:01 +0000

    implemented typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+const increment = 50;
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  },delay);
+  delay += increment;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+},delay);
